f139a5febdce774509bad574f52af414280f4d13
Vikingman <drake@popalot.net>
2023-08-11 03:28:29 -0400

    Base Cannon shoot at anybody

--- BaseCanon.gd
+++ BaseCanon.gd
@@ -1,6 +1,6 @@
 extends MeshInstance3D
 
-
+var laser = preload("res://weapons/laser.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,3 +10,17 @@ func _ready():
 func _process(delta):
 	rotate_z(0.01)
 	pass
+
+@rpc("call_local", "any_peer")
+func shoot(id):
+	var inst = laser.instantiate()
+	inst.position = $Guns.global_position
+	inst.rotation = rotation
+	inst.name = id
+	
+	get_tree().get_current_scene().get_node("Space/Spawner").add_child(inst)
+
+func _on_area_3d_body_entered(body):
+	shoot.rpc(str(rid_allocate_id()))
+	print("shoot")
+	pass # Replace with function body.
